30dd314858243140cc51bfb48776e616f6621c37
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-11 15:54:10 +0100

    Aggiunta immagine alla pagina presa da un data

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        message: 'Hello world!'
+        message: 'Hello world!',
+        image: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
       }
     }
   }).mount('#app')
